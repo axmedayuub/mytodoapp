0ca4a458a8015046c16a942c5d37eb4e960552d1
axmedyuub <axmed.m.ayuub@gmail.com>
2021-07-07 21:21:32 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import "./Style.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-// Put any other imports below so that CSS from your
-// components takes precedence over default styles.
 ReactDOM.render(
   <React.StrictMode>
     <App />
